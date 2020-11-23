c6ba3fe493833e37fc404d764ad33138b0d2003a
temmietope <temmieayodele@gmail.com>
2020-11-23 17:34:02 +0100

    added images

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,10 @@
 import React from "react"
-import { Container } from "../components"
+import { Container, FeatureImage } from "../components"
 
 const IndexPage = () => {
-  return <Container></Container>
+  return <Container>
+    <FeatureImage/>
+  </Container>
 }
 
 export default IndexPage
