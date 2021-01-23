093202adb4889e6144dcca9efef9e08bd36b5382
temmietope <temmieayodele@gmail.com>
2021-01-23 07:21:10 +0100

    added html language

--- src/components/Seo.js
+++ src/components/Seo.js
@@ -24,6 +24,7 @@ export const Seo = ({ description, keywords, title, image, url, author }) => {
         ]
         return (
           <Helmet
+            htmlAttributes={{ lang: "en" }}
             title={metaTitle}
             link={[
               { rel: "shortcut-icon", type: "image/png", href: `${favicon}` },
